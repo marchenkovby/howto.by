1068bfd7f0cf14e6df8946d9d79b844c3a6e0b84
Mac <marchenkovby@gmail.com>
2022-05-17 12:05:41 +0300

    Update images.js

--- tasks/images.js
+++ tasks/images.js
@@ -1,6 +1,15 @@
 const { src, dest } = require('gulp');
+const imagemin = require('gulp-imagemin');
+const cache = require('gulp-cache');
 
 module.exports = function images() {
-  //	return src('app/src/images/**/*') // Берём все изображения из папки источника
-  //	.pipe(dest('app/build/public_html/images/')) // Выгружаем оптимизированные изображения в папку назначения
+  return src('app/src/images/**/*')
+  .pipe(cache(imagemin()))
+  .pipe(dest('app/build/public_html/images/**/*'))
 }
+
+// Попробовать убрать module.exports, а также const { src, dest } = require('gulp')
+//
+// npm install gulp-imagemin --save-dev
+// npm install gulp-cache --save-dev
+// https://webref.ru/dev/gulp-for-beginners/optimizing-images
