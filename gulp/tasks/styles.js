951dc90c9bfefd05f29b9eeb9a17a2d5c92a0295
Maksim <marchenkovby@gmail.com>
2022-05-19 22:48:41 +0300

    Update Gulp

--- gulp/tasks/styles.js
+++ gulp/tasks/styles.js
@@ -4,8 +4,8 @@ import csscomb from 'gulp-csscomb';
 import rename from 'gulp-rename';
 import cleanCss from 'gulp-clean-css';
 import autoprefixer from 'gulp-autoprefixer';
-//import sassGlob from 'gulp-sass-glob';
-//import cssBeautify from 'gulp-cssbeautify';
+import sassGlob from 'gulp-sass-glob';
+import cssbeautify from 'gulp-cssbeautify';
 
 const sass = gulpSass(dartSass);
 
@@ -17,22 +17,20 @@ export const styles = () => {
       }))
     )
     //.pipe(app.plugins.replace(/@img\//g, '../images/'))
-    //.pipe(sassGlob())
-    .pipe(sass({
-      outputStyle: 'expanded'
+    .pipe(sassGlob())
+    .pipe(sass())
+    .pipe(cssbeautify())
+    .pipe(autoprefixer({
+      grid: true,
+      overrideBrowserslist: ['last 3 versions']
     }))
-    //.pipe(cssBeautify())
-    // .pipe(autoprefixer({
-    //   grid: true,
-    //   overrideBrowserslist: ['last 3 versions']
-    // }))
     .pipe(csscomb())
-    // .pipe(app.plugins.replace('@charset "UTF-8";', ''))
-    // .pipe(app.gulp.dest(app.path.build.css))
-    // .pipe(rename({
-    //    extname: '.min.css'
-    // }))
-    // .pipe(cleanCss())
+    .pipe(app.plugins.replace(/@charset "UTF-8";\n\n/, ''))
     .pipe(app.gulp.dest(app.path.build.css))
-    // .pipe(app.plugins.browsersync.stream())
+    .pipe(rename({
+      extname: '.min.css'
+    }))
+    .pipe(cleanCss())
+    .pipe(app.gulp.dest(app.path.build.css))
+    .pipe(app.plugins.browsersync.stream())
 }
